ab18060ff2e3913a37c1646f1f41448615575a2a
zhiyi <ongzhiyi@live.com>
2021-03-17 00:46:28 +0800

    revamping the bot part 1

--- dad_bot.py
+++ dad_bot.py
@@ -18,70 +18,91 @@ logger = logging.getLogger(__name__)
 # PORT = int(os.environ.get('PORT', '443'))
 
 def dad_reply(update, context):
-    message = update.message.text.lower()
-    message = re.sub('[^A-Za-z0-9]+', ' ', message)
-    print(message)
-    imsub1 = "im "
-    imsub4 = " im"
-    imsub2 = "i am"
-    imsub3 = "i m "
-    imsub5 = " i m"
-    dad = "dad"
 
-    if imsub1 in message or imsub4 in message:
-        subindex = message.index(imsub1)
-        identity = message[subindex + 2:]
+    # function one: 'hi {}, im dad' 
+
+    text = update.message.text
+    words = text.lower().split()
+
+    for word in words:
+        word = re.sub('[^A-Za-z0-9]+', '', word)
+
+    if "im" in words:
+        subindex = text.index("im")
+        identity = text[subindex + 2:]
         print(identity)
         update.message.reply_text("Hi" + identity + ", I'm Dad!")
 
-    elif imsub2 in message:
-        subindex = message.index(imsub2)
-        identity = message[subindex + 4:]
+    if "i" in words and "am" in words:
+        subindex = text.index("am")
+        identity = text[subindex + 2:]
         print(identity)
         update.message.reply_text("Hi" + identity + ", I'm Dad!")
-    
-    elif imsub3 in message or imsub5 in message:
-        subindex = message.index(imsub3)
-        identity = message[subindex + 3:]
-        print(identity)
-        update.message.reply_text("Hi" + identity + ", I'm Dad!")
-    
-    elif dad in message:
-        update.message.reply_text("Ya?")
+        
+    # message = update.message.text.lower()
+    # message = re.sub('[^A-Za-z0-9]+', ' ', message)
+    # print(message)
+    # imsub1 = "im "
+    # imsub4 = " im"
+    # imsub2 = "i am"
+    # imsub3 = "i m "
+    # imsub5 = " i m"
+    # dad = "dad"
 
-    message2 = update.message.text.lower()
-    message2 = re.sub('[^A-Za-z0-9]+', '', message2)
-    shutupsub1 = "shutup"
-    shutupsub2 = "stfu"
-    shutupsub3 = "shaddup"
-    shutupsub4 = "shutthefuckup"
-    dad = "imdad"
-    hidad1 = "hidad"
-    hidad2 = "hellodad"
-    byedad = "byedad"
-    if shutupsub1 in message2 or shutupsub2 in message2 or shutupsub3 in message or shutupsub4 in message:
-        update.message.reply_text("Listen here " + update.message.from_user['first_name'] + ", I will not tolerate you saying the words that consist any variation of the letters 's h u t  u p' being said in this server, so take your own advice and close thine mouth in the name of the christian minecraft server owner.")
-    
-    if dad in message2:
-        update.message.reply_text("No you're not, I'm Dad.")
+    # if imsub1 in message or imsub4 in message:
+    #     subindex = message.index(imsub1)
+    #     identity = message[subindex + 2:]
+    #     print(identity)
+    #     update.message.reply_text("Hi" + identity + ", I'm Dad!")
 
-    if hidad1 in message2 or hidad2 in message2:
-        update.message.reply_text("Sorry ah, laopei busy, go entertain yourself first.")
+    # elif imsub2 in message:
+    #     subindex = message.index(imsub2)
+    #     identity = message[subindex + 4:]
+    #     print(identity)
+    #     update.message.reply_text("Hi" + identity + ", I'm Dad!")
     
-    if byedad in message2:
-        update.message.reply_text("Ok come home by 10pm ah.")
+    # elif imsub3 in message or imsub5 in message:
+    #     subindex = message.index(imsub3)
+    #     identity = message[subindex + 3:]
+    #     print(identity)
+    #     update.message.reply_text("Hi" + identity + ", I'm Dad!")
+    
+    # elif dad in message:
+    #     update.message.reply_text("Ya?")
 
-    message3 = update.message.text.lower()
-    message3 = re.sub('[^A-Za-z0-9]+', '', message3)
-    playsub1 = "play"
-    playsub2 = "playing"
-    playsub3 = "playin"
+    # message2 = update.message.text.lower()
+    # message2 = re.sub('[^A-Za-z0-9]+', '', message2)
+    # shutupsub1 = "shutup"
+    # shutupsub2 = "stfu"
+    # shutupsub3 = "shaddup"
+    # shutupsub4 = "shutthefuckup"
+    # dad = "imdad"
+    # hidad1 = "hidad"
+    # hidad2 = "hellodad"
+    # byedad = "byedad"
+    # if shutupsub1 in message2 or shutupsub2 in message2 or shutupsub3 in message or shutupsub4 in message:
+    #     update.message.reply_text("Listen here " + update.message.from_user['first_name'] + ", I will not tolerate you saying the words that consist any variation of the letters 's h u t  u p' being said in this server, so take your own advice and close thine mouth in the name of the christian minecraft server owner.")
     
-    if playsub2 in message3 or playsub3 in message3:
-        update.message.reply_text("Are ya winnin' son?")
+    # if dad in message2:
+    #     update.message.reply_text("No you're not, I'm Dad.")
+
+    # if hidad1 in message2 or hidad2 in message2:
+    #     update.message.reply_text("Sorry ah, laopei busy, go entertain yourself first.")
     
-    elif playsub1 in message3:
-        update.message.reply_text("I hope ya win, son!")
+    # if byedad in message2:
+    #     update.message.reply_text("Ok come home by 10pm ah.")
+
+    # message3 = update.message.text.lower()
+    # message3 = re.sub('[^A-Za-z0-9]+', '', message3)
+    # playsub1 = "play"
+    # playsub2 = "playing"
+    # playsub3 = "playin"
+    
+    # if playsub2 in message3 or playsub3 in message3:
+    #     update.message.reply_text("Are ya winnin' son?")
+    
+    # elif playsub1 in message3:
+    #     update.message.reply_text("I hope ya win, son!")
 
 def error(update, context):
     """Log Errors caused by Updates."""
